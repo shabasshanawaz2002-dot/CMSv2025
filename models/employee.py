5c87625fb3d7fd9b2a37bcf6b68988fc54751269
Athul <athulsatheesh02@gmail.com>
2025-11-29 13:23:06 +0530

    cons modified

--- models/employee.py
+++ models/employee.py
@@ -1,4 +1,6 @@
-class Employee:
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
+class employee:
+    def __init__(self, name , age ):
+        self.name=name
+        self.age=age
+emp1=employee("Athul",23)
+print(f"employee name is {employee.name} and age is {employee.age}")
