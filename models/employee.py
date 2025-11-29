48c5ace848e5ad71992e0fdbece3ffeb45463c08
Athul <athulsatheesh02@gmail.com>
2025-11-29 13:16:37 +0530

    constructor add

--- models/employee.py
+++ models/employee.py
@@ -1,2 +1,6 @@
-class Employee:
-    pass
+class employee:
+    def __init__(self, name , age ):
+        self.name=name
+        self.age=age
+emp1=employee("Athul",23)
+print(f"employee name is {employee.name} and age is {employee.age}")
