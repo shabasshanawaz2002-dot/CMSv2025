d7857fa9a024a3f45ced690016276cd244b4b93c
Athul <athulsatheesh02@gmail.com>
2025-11-29 15:52:02 +0530

    emp updated

--- models/employee.py
+++ models/employee.py
@@ -1,6 +1,2 @@
-class employee:
-    def __init__(self, name , age ):
-        self.name=name
-        self.age=age
-emp1=employee("Athul",23)
-print(f"employee name is {employee.name} and age is {employee.age}")
+class Employee:
+    pass
